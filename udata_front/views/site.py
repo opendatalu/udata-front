876e991b0a65350999ec0d86bbc91b99db8aa5f5
JFK <jean-francois.kener@lemonads.com>
2022-07-06 10:31:42 +0200

    adapted social links

--- udata_front/views/site.py
+++ udata_front/views/site.py
@@ -28,7 +28,7 @@ from udata_front import theme
 
 from udata.core.site.models import current_site
 
-blueprint = I18nBlueprint('site', __name__)
+blueprint = I18nBlueprint("site", __name__)
 
 log = logging.getLogger(__name__)
 
@@ -38,15 +38,12 @@ def inject_site():
     return dict(current_site=current_site)
 
 
-@blueprint.route('/activity.atom')
+@blueprint.route("/activity.atom")
 def activity_feed():
-    activity_keys = request.args.getlist('key')
+    activity_keys = request.args.getlist("key")
 
-    feed = AtomFeed(
-        current_app.config.get('SITE_TITLE'), feed_url=request.url,
-        url=request.url_root)
-    activities = (Activity.objects.order_by('-created_at')
-                                  .limit(current_site.feed_size))
+    feed = AtomFeed(current_app.config.get("SITE_TITLE"), feed_url=request.url, url=request.url_root)
+    activities = Activity.objects.order_by("-created_at").limit(current_site.feed_size)
 
     for activity in activities.select_related():
         # filter by activity.key
@@ -56,51 +53,46 @@ def activity_feed():
         try:
             owner = activity.actor or activity.organization
         except DoesNotExist:
-            owner = 'deleted'
+            owner = "deleted"
             owner_url = None
         else:
             owner_url = owner.url_for(_external=True)
         try:
             related = activity.related_to
         except DoesNotExist:
-            related = 'deleted'
+            related = "deleted"
             related_url = None
         else:
             related_url = related.url_for(_external=True)
         feed.add(
-            id='%s#activity=%s' % (
-                url_for('site.dashboard', _external=True), activity.id),
-            title='%s by %s on %s' % (
-                activity.key, owner, related),
+            id="%s#activity=%s" % (url_for("site.dashboard", _external=True), activity.id),
+            title="%s by %s on %s" % (activity.key, owner, related),
             url=related_url,
-            author={
-                'name': owner,
-                'uri': owner_url,
-            },
-            updated=activity.created_at
+            author={"name": owner, "uri": owner_url,},
+            updated=activity.created_at,
         )
     return feed.get_response()
 
 
-@blueprint.route('/')
+@blueprint.route("/")
 def home():
     context = {
-        'spd_datasets': Dataset.objects.filter(badges__kind='spd'),
-        'recent_datasets': Dataset.objects.visible(),
-        'recent_reuses': Reuse.objects(featured=True).visible(),
-        'last_post': Post.objects.published().first()
+        "spd_datasets": Dataset.objects.filter(badges__kind="spd"),
+        "recent_datasets": Dataset.objects.visible(),
+        "recent_reuses": Reuse.objects(featured=True).visible(),
+        "last_post": Post.objects.published().first(),
     }
-    processor = theme.current.get_processor('home')
+    processor = theme.current.get_processor("home")
     context = processor(context)
-    return theme.render('home.html', **context)
+    return theme.render("home.html", **context)
 
 
 def get_export_url(model):
-    did = current_app.config['EXPORT_CSV_DATASET_ID']
+    did = current_app.config["EXPORT_CSV_DATASET_ID"]
     dataset = Dataset.objects.get_or_404(id=did)
     resource = None
     for r in dataset.resources:
-        if r.extras.get('csv-export:model', '') == model:
+        if r.extras.get("csv-export:model", "") == model:
             resource = r
             break
     if not resource:
@@ -108,65 +100,63 @@ def get_export_url(model):
     return resource.url
 
 
-@blueprint.route('/datasets.csv')
+@blueprint.route("/datasets.csv")
 def datasets_csv():
     params = multi_to_dict(request.args)
     # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not params and 'dataset' in exported_models:
-        return redirect(get_export_url('dataset'))
-    params['facets'] = False
+    exported_models = current_app.config.get("EXPORT_CSV_MODELS", [])
+    if not params and "dataset" in exported_models:
+        return redirect(get_export_url("dataset"))
+    params["facets"] = False
     datasets = DatasetApiParser.parse_filters(Dataset.objects.visible(), params)
     adapter = csv.get_adapter(Dataset)
-    return csv.stream(adapter(datasets), 'datasets')
+    return csv.stream(adapter(datasets), "datasets")
 
 
-@blueprint.route('/resources.csv')
+@blueprint.route("/resources.csv")
 def resources_csv():
     params = multi_to_dict(request.args)
     # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not params and 'resource' in exported_models:
-        return redirect(get_export_url('resource'))
-    params['facets'] = False
+    exported_models = current_app.config.get("EXPORT_CSV_MODELS", [])
+    if not params and "resource" in exported_models:
+        return redirect(get_export_url("resource"))
+    params["facets"] = False
     datasets = DatasetApiParser.parse_filters(Dataset.objects.visible(), params)
-    return csv.stream(ResourcesCsvAdapter(datasets), 'resources')
+    return csv.stream(ResourcesCsvAdapter(datasets), "resources")
 
 
-@blueprint.route('/organizations.csv')
+@blueprint.route("/organizations.csv")
 def organizations_csv():
     params = multi_to_dict(request.args)
     # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not params and 'organization' in exported_models:
-        return redirect(get_export_url('organization'))
-    params['facets'] = False
+    exported_models = current_app.config.get("EXPORT_CSV_MODELS", [])
+    if not params and "organization" in exported_models:
+        return redirect(get_export_url("organization"))
+    params["facets"] = False
     organizations = OrgApiParser.parse_filters(Organization.objects.visible(), params)
-    return csv.stream(OrganizationCsvAdapter(organizations), 'organizations')
+    return csv.stream(OrganizationCsvAdapter(organizations), "organizations")
 
 
-@blueprint.route('/reuses.csv')
+@blueprint.route("/reuses.csv")
 def reuses_csv():
     params = multi_to_dict(request.args)
     # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not params and 'reuse' in exported_models:
-        return redirect(get_export_url('reuse'))
-    params['facets'] = False
+    exported_models = current_app.config.get("EXPORT_CSV_MODELS", [])
+    if not params and "reuse" in exported_models:
+        return redirect(get_export_url("reuse"))
+    params["facets"] = False
     reuses = ReuseApiParser.parse_filters(Reuse.objects.visible(), params)
-    return csv.stream(ReuseCsvAdapter(reuses), 'reuses')
+    return csv.stream(ReuseCsvAdapter(reuses), "reuses")
 
 
-@blueprint.route('/harvests.csv')
+@blueprint.route("/harvests.csv")
 def harvests_csv():
     # redirect to EXPORT_CSV dataset if feature is enabled
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if 'harvest' in exported_models:
-        return redirect(get_export_url('harvest'))
-    adapter = HarvestSourceCsvAdapter(
-        HarvestSource.objects.filter(deleted=None).order_by('created_at')
-    )
-    return csv.stream(adapter, 'harvest')
+    exported_models = current_app.config.get("EXPORT_CSV_MODELS", [])
+    if "harvest" in exported_models:
+        return redirect(get_export_url("harvest"))
+    adapter = HarvestSourceCsvAdapter(HarvestSource.objects.filter(deleted=None).order_by("created_at"))
+    return csv.stream(adapter, "harvest")
 
 
 class SiteView(object):
@@ -177,59 +167,38 @@ class SiteView(object):
     object = site
 
 
-@blueprint.route('/dashboard/', endpoint='dashboard')
+@blueprint.route("/dashboard/", endpoint="dashboard")
 class SiteDashboard(SiteView, DetailView):
-    template_name = 'site/dashboard.html'
+    template_name = "site/dashboard.html"
 
     def get_context(self):
         context = super(SiteDashboard, self).get_context()
 
-        context['metrics'] = ({
-            'title': _('Data'),
-            'widgets': [
-                {
-                    'title': _('Datasets'),
-                    'metric': 'datasets',
-                    'type': 'line',
-                    'endpoint': 'datasets.list',
-                },
-                {
-                    'title': _('Reuses'),
-                    'metric': 'reuses',
-                    'type': 'line',
-                    'endpoint': 'reuses.list',
-                },
-                {
-                    'title': _('Resources'),
-                    'metric': 'resources',
-                    'type': 'line',
-                }
-            ]
-        }, {
-            'title': _('Community'),
-            'widgets': [
-                {
-                    'title': _('Organizations'),
-                    'metric': 'organizations',
-                    'type': 'bar',
-                    'endpoint': 'organizations.list',
-                },
-                {
-                    'title': _('Users'),
-                    'metric': 'users',
-                    'type': 'bar',
-                    'endpoint': 'users.list'
-                }
-            ]
-        })
+        context["metrics"] = (
+            {
+                "title": _("Data"),
+                "widgets": [
+                    {"title": _("Datasets"), "metric": "datasets", "type": "line", "endpoint": "datasets.list",},
+                    {"title": _("Reuses"), "metric": "reuses", "type": "line", "endpoint": "reuses.list",},
+                    {"title": _("Resources"), "metric": "resources", "type": "line",},
+                ],
+            },
+            {
+                "title": _("Community"),
+                "widgets": [
+                    {"title": _("Organizations"), "metric": "organizations", "type": "bar", "endpoint": "organizations.list",},
+                    {"title": _("Users"), "metric": "users", "type": "bar", "endpoint": "users.list"},
+                ],
+            },
+        )
 
         return context
 
 
 @cache.cached(50)
 def get_terms_content():
-    filename = current_app.config['SITE_TERMS_LOCATION']
-    if filename.startswith('http'):
+    filename = current_app.config["SITE_TERMS_LOCATION"]
+    if filename.startswith("http"):
         # We let the error appear because:
         # - we dont want to cache false responses
         # - this is only visible on terms
@@ -241,14 +210,14 @@ def get_terms_content():
             return terms.read()
 
 
-@blueprint.route('/terms/')
+@blueprint.route("/terms/")
 def terms():
     content = get_terms_content()
-    return theme.render('terms.html', terms=content)
+    return theme.render("terms.html", terms=content)
 
 
 @sitemap.register_generator
 def site_sitemap_urls():
-    yield 'site.home_redirect', {}, None, 'daily', 1
-    yield 'site.dashboard_redirect', {}, None, 'weekly', 0.6
-    yield 'site.terms_redirect', {}, None, 'monthly', 0.2
+    yield "site.home_redirect", {}, None, "daily", 1
+    yield "site.dashboard_redirect", {}, None, "weekly", 0.6
+    yield "site.terms_redirect", {}, None, "monthly", 0.2
