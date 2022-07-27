b472ed82893dfe5a567281bb3a9f39e031244e9f
Jean-François Kéner <JJL762@gouv.etat.lu>
2022-07-27 11:16:33 +0200

    Added missing endpoints in footer links

--- udata_front/theme/gouvfr/__init__.py
+++ udata_front/theme/gouvfr/__init__.py
@@ -67,10 +67,10 @@ theme.menu(gouvfr_menu)
 opendata_links = [
     nav.Item(_("FAQ"), "gouvlu.faq"),
     nav.Item(_("Guide for the usage of open data"), "gouvlu.usage"),
-    nav.Item(_("Guide for publishing open data"), args={"slug": "topics/publishing"}),
+    nav.Item(_("Guide for publishing open data"), "gouvfr.show_page", args={"slug": "topics/publishing"}),
     nav.Item(_("Request data for its reuse"), "gouvlu.requesting"),
     nav.Item(_("Strategy of open data"), "gouvfr.show_page", args={"slug": "topics/strategy"}),
-    nav.Item(_("Railroad sheet of open data"), args={"slug": "topics/5yearplan"}),
+    nav.Item(_("Railroad sheet of open data"), "gouvfr.show_page", args={"slug": "topics/5yearplan"}),
     nav.Item(_("Terms of use"), "site.terms"),
     # nav.Item(_("Featured topics"), "gouvfr.show_page", args={"slug": "thematiques-a-la-une"}),
     # nav.Item(_("Reference Data"), "gouvfr.show_page", args={"slug": "spd/reference"}),
