1e302c5399c149c4585b53450be3d76d509064ec
Jean-François Kéner <JJL762@gouv.etat.lu>
2023-01-12 15:39:37 +0100

    Fix password -> password_confirm

--- theme/js/patches/form_messages.js
+++ theme/js/patches/form_messages.js
@@ -83,7 +83,7 @@ function loadDOMElements() {
   EMAIL_INPUT = document.getElementById("email");
   PASSWORD_INPUT = document.getElementById("password");
   if (CURRENT_PAGE === "/register") {
-    PASSWORD_CONFIRM_INPUT = document.getElementById("password");
+    PASSWORD_CONFIRM_INPUT = document.getElementById("password_confirm");
     FIRST_NAME_INPUT = document.getElementById("first_name-id");
     LAST_NAME_INPUT = document.getElementById("last_name-id");
   }
