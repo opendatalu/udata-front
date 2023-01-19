bdb5ad2e731453f9404ce9077429353b2c3a4732
Jean-François Kéner <JJL762@gouv.etat.lu>
2023-01-19 13:17:10 +0100

    [#90] Text inside <p>

--- theme/js/components/dataset/resource/resource.vue
+++ theme/js/components/dataset/resource/resource.vue
@@ -5,37 +5,47 @@
       :id="'resource-' + resource.id + '-header'"
     >
       <div class="fr-col-auto fr-grid-row fr-grid-row--top no-wrap">
-        <div class="fr-col-auto fr-mx-2w fr-icon-svg fr-icon--sm" v-html="resourceImage" aria-hidden="true"></div>
+        <div
+          class="fr-col-auto fr-mx-2w fr-icon-svg fr-icon--sm"
+          v-html="resourceImage"
+          aria-hidden="true"
+        ></div>
         <div class="fr-col-auto">
-          <h4
-            class="fr-mb-1v"
-            :id="'resource-' + resource.id + '-title'"
-          >
-            {{ resource.title || $t('Nameless resource') }}
+          <h4 class="fr-mb-1v" :id="'resource-' + resource.id + '-title'">
+            {{ resource.title || $t("Nameless resource") }}
           </h4>
           <div class="fr-text--sm fr-mb-0 text-grey-380">
-            <template v-if="resource.owner">
-              {{ $t('From') }} {{owner}} —
-            </template>
-            <template v-else-if="resource.organization">
-              {{ $t('From') }} <a :href="resource.organization.page">{{ owner }}</a> —
-            </template>
-            {{$t('Updated on X', {date: filters.formatDate(lastUpdate)})}} —
-            <template v-if="resource.format">
-              {{ resource.format?.trim()?.toLowerCase() }}
-              <template v-if="resource.filesize">({{ filters.filesize(resource.filesize) }})</template> —
-            </template>
-            {{ $t('X downloads', resource.metrics.views || 0) }}
+            <p style="margin: 0">
+              <template v-if="resource.owner">
+                {{ $t("From") }} {{ owner }} —
+              </template>
+              <template v-else-if="resource.organization">
+                {{ $t("From") }}
+                <a :href="resource.organization.page">{{ owner }}</a> —
+              </template>
+              {{
+                $t("Updated on X", { date: filters.formatDate(lastUpdate) })
+              }}
+              —
+              <template v-if="resource.format">
+                {{ resource.format?.trim()?.toLowerCase() }}
+                <template v-if="resource.filesize"
+                  >({{ filters.filesize(resource.filesize) }})</template
+                >
+                —
+              </template>
+              {{ $t("X downloads", resource.metrics.views || 0) }}
+            </p>
           </div>
         </div>
       </div>
       <div class="fr-col-auto fr-ml-6w fr-ml-md-0">
         <ul class="fr-grid-row fr-grid-row--middle no-wrap wrap-md">
           <li class="text-default-error fr-mr-5w" v-if="unavailable">
-            {{$t('Unavailable')}}
+            {{ $t("Unavailable") }}
           </li>
           <li class="fr-col-auto fr-mr-5w" v-if="showSchemaButton">
-            <schema-button :resource="resource"/>
+            <schema-button :resource="resource" />
           </li>
           <li class="fr-col-auto fr-mr-3v" v-if="canEdit">
             <EditButton
@@ -47,11 +57,16 @@
           <li class="fr-col-auto fr-mr-3v" v-if="resource.preview_url">
             <button
               :title="$t('Preview')"
-              @click.prevent="showModal('preview', {url: resource.preview_url, title: resource.title}, true)"
+              @click.prevent="
+                showModal(
+                  'preview',
+                  { url: resource.preview_url, title: resource.title },
+                  true
+                )
+              "
               class="fr-btn fr-btn--secondary fr-btn--secondary-grey-500 fr-btn--sm fr-icon-svg"
               v-html="preview"
-            >
-            </button>
+            ></button>
           </li>
           <li class="fr-col-auto" v-if="resource.format === 'url'">
             <a
@@ -80,69 +95,86 @@
               :title="$t('See more details')"
               :aria-controls="'resource-' + resource.id"
               class="accordion-button rounded-circle fr-icon-arrow-right-s-line fr-p-1w"
-            >
-            </button>
+            ></button>
           </li>
         </ul>
       </div>
     </header>
     <section
       class="accordion-content fr-pt-5v fr-pb-4w fr-pl-6w"
-      :class="{active: expanded}"
-      :style="{height: expanded ? 'auto' : 0}"
+      :class="{ active: expanded }"
+      :style="{ height: expanded ? 'auto' : 0 }"
       :aria-labelledby="'resource-' + resource.id + '-title'"
       :hidden="!expanded"
       :id="`resource-${resource.id}-section`"
     >
-      <div class=" fr-mt-0 markdown" v-if="resource.description" v-html="filters.markdown(resource.description)">
-      </div>
+      <div
+        class="fr-mt-0 markdown"
+        v-if="resource.description"
+        v-html="filters.markdown(resource.description)"
+      ></div>
       <dl>
         <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t('URL') }}</dt>
+          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t("URL") }}</dt>
           <dd class="fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10">
-            <a :href="resource.url">{{resource.url}}</a>
+            <a :href="resource.url">{{ resource.url }}</a>
           </dd>
         </div>
         <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t('Permalink') }}</dt>
+          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">
+            {{ $t("Permalink") }}
+          </dt>
           <dd class="fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10">
-            <a :href="resource.latest">{{resource.latest}}</a>
+            <a :href="resource.latest">{{ resource.latest }}</a>
           </dd>
         </div>
         <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t('Type') }}</dt>
+          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t("Type") }}</dt>
           <dd class="fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10">
             {{ typeLabel }}
           </dd>
         </div>
         <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t('MIME Type') }}</dt>
+          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">
+            {{ $t("MIME Type") }}
+          </dt>
           <dd class="fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10">
-            {{resource.mime}}
+            {{ resource.mime }}
           </dd>
         </div>
-        <div v-if="resource.checksum" class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{resource.checksum.type}}</dt>
+        <div
+          v-if="resource.checksum"
+          class="fr-grid-row fr-grid-row--gutters fr-mb-2w"
+        >
+          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">
+            {{ resource.checksum.type }}
+          </dt>
           <dd class="fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10">
-            {{resource.checksum.value}}
+            {{ resource.checksum.value }}
           </dd>
         </div>
         <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t('Created on') }}</dt>
+          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">
+            {{ $t("Created on") }}
+          </dt>
           <dd class="fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10">
-            {{filters.formatDate(resource.created_at)}}
+            {{ filters.formatDate(resource.created_at) }}
           </dd>
         </div>
         <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t('Modified on') }}</dt>
+          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">
+            {{ $t("Modified on") }}
+          </dt>
           <dd class="fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10">
-            {{filters.formatDate(resource.last_modified)}}
+            {{ filters.formatDate(resource.last_modified) }}
           </dd>
         </div>
         <div class="fr-grid-row fr-grid-row--gutters">
-          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">{{ $t('Published on') }}</dt>
+          <dt class="fr-col-4 fr-col-md-3 fr-col-lg-2">
+            {{ $t("Published on") }}
+          </dt>
           <dd class="fr-ml-0 fr-col-8 fr-col-md-9 fr-col-lg-10">
-            {{filters.formatDate(resource.published)}}
+            {{ filters.formatDate(resource.published) }}
           </dd>
         </div>
       </dl>
@@ -156,10 +188,10 @@ import SchemaButton from "./schema-button.vue";
 import useOwnerName from "../../../composables/useOwnerName";
 import useResourceImage from "../../../composables/useResourceImage";
 import EditButton from "./edit-button.vue";
-import preview from 'bundle-text:svg/preview.svg';
+import preview from "bundle-text:svg/preview.svg";
 
 export default defineComponent({
-  components: {EditButton, SchemaButton},
+  components: { EditButton, SchemaButton },
   props: {
     datasetId: {
       type: String,
@@ -185,14 +217,24 @@ export default defineComponent({
   setup(props) {
     const owner = useOwnerName(props.resource);
     const resourceImage = useResourceImage(props.resource);
-    const filters = inject('$filters');
-    const showModal = inject('$showModal');
+    const filters = inject("$filters");
+    const showModal = inject("$showModal");
     const expanded = ref(false);
-    const expand = () => expanded.value = !expanded.value;
-    const availabilityChecked = computed(() => props.resource.extras && props.resource.extras['check:status']);
-    const lastUpdate = computed(() => props.resource.published > props.resource.last_modified ? props.resource.published : props.resource.last_modified);
-    const unavailable = computed(() => availabilityChecked.value && availabilityChecked.value >= 400);
-    const showSchemaButton = computed(() => props.resource.schema && props.resource.schema.name);
+    const expand = () => (expanded.value = !expanded.value);
+    const availabilityChecked = computed(
+      () => props.resource.extras && props.resource.extras["check:status"]
+    );
+    const lastUpdate = computed(() =>
+      props.resource.published > props.resource.last_modified
+        ? props.resource.published
+        : props.resource.last_modified
+    );
+    const unavailable = computed(
+      () => availabilityChecked.value && availabilityChecked.value >= 400
+    );
+    const showSchemaButton = computed(
+      () => props.resource.schema && props.resource.schema.name
+    );
     return {
       owner,
       resourceImage,
@@ -205,7 +247,7 @@ export default defineComponent({
       unavailable,
       showSchemaButton,
       preview,
-    }
+    };
   },
 });
 </script>
